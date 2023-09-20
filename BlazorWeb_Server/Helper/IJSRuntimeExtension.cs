8963c48305440db9d0030701a372e4f1c2fcd264
Teo <8duchuynh@gmail.com>
2023-09-20 20:10:41 +0700

    blazor lifecycle

---
+++ BlazorWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToaStrSuccess(this IJSRuntime jSRuntime,string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToaStrFail(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr",  "error", message);
+        }
+    }
+}

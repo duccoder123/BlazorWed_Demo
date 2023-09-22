9204781fd45196e943d334b77df590e1d0308254
Teo <8duchuynh@gmail.com>
2023-09-22 23:22:31 +0700

    display category ui

--- BlazorDemo_Models/CategoryDTO.cs
+++ BlazorDemo_Models/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace BlazorDemo_Models
 {
     public class CategoryDTO
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [Required(ErrorMessage= "Please enter your name")]
         public string Name { get; set; }    
     }
 }
